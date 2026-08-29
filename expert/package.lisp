862e5496c90c928588fd5d256c8c60ef907b0284
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-29 12:15:36 -0400

    feat: expose expert substrate operations

--- expert/package.lisp
+++ expert/package.lisp
@@ -6,13 +6,24 @@
                 #:new-database
                 #:open-database
                 #:close-database
-                #:db-is-open-p)
+                #:db-is-open-p
+                #:fetch*
+                #:put*
+                #:with-write-transaction)
   (:export
    #:expert-host
    #:expert-host-data-directory
    #:expert-host-database
    #:expert-host-prolog-process
    #:expert-host-prolog-session-id
+   #:expert-host-prolog-request-sequence
    #:start-expert-host
    #:stop-expert-host
-   #:expert-host-open-p))
+   #:expert-host-open-p
+   #:current-kb-revision
+   #:project-request-event
+   #:fetch-request-event
+   #:derive-event-transport
+   #:dispatch-expert-request
+   #:serve-stdio
+   #:main))
